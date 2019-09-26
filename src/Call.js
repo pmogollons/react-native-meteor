c2a51fe3fbf5cbb5d0e6a0a249055782b7dc78d7
Paulo Mogollón <pmogollon@unbosque.edu.co>
2019-09-26 11:28:36 -0500

    Return promise id no callback on call

--- src/Call.js
+++ src/Call.js
@@ -1,14 +1,26 @@
 import Data from './Data';
 
 export default function(eventName) {
-  var args = Array.prototype.slice.call(arguments, 1);
+  const args = Array.prototype.slice.call(arguments, 1);
+  const id = Data.ddp.method(eventName, args);
+  let callback;
+
   if (args.length && typeof args[args.length - 1] === 'function') {
-    var callback = args.pop();
+    callback = args.pop();
+  } else {
+    return new Promise((resolve, reject) => {
+      Data.calls.push({
+        id: id,
+        callback(error, response) {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(response);
+          }
+        },
+      });
+    });
   }
 
-  const id = Data.ddp.method(eventName, args);
-  Data.calls.push({
-    id: id,
-    callback: callback,
-  });
+  Data.calls.push({ id: id, callback });
 }
