4de8bacdfc5712c857a169ff978c55e9021a1213
Paulo Mogollón <pmogollon@unbosque.edu.co>
2019-10-27 13:42:07 -0500

    Added timeout to method calls

--- src/Call.js
+++ src/Call.js
@@ -1,26 +1,61 @@
-import Data from './Data';
+import NetInfo from '@react-native-community/netinfo';
 
-export default function(eventName) {
+import Data from './Data';
+import MeteorError from '../lib/MeteorError';
+
+export default function(eventName, props, config = {}) {
   const args = Array.prototype.slice.call(arguments, 1);
-  const id = Data.ddp.method(eventName, args);
-  let callback;
 
   if (args.length && typeof args[args.length - 1] === 'function') {
-    callback = args.pop();
-  } else {
-    return new Promise((resolve, reject) => {
-      Data.calls.push({
-        id: id,
-        callback(error, response) {
-          if (error) {
-            reject(error);
-          } else {
-            resolve(response);
-          }
-        },
-      });
-    });
+    const id = Data.ddp.method(eventName, args);
+    const callback = args.pop();
+
+    return Data.calls.push({id: id, callback});
   }
 
-  Data.calls.push({ id: id, callback });
+  let timeoutId;
+
+  const callPromise = new Promise((resolve, reject) => {
+    NetInfo.fetch()
+      .then(netStatus => {
+        if (!netStatus.isConnected) {
+          return reject(
+            new MeteorError('NO_CONNECTION', 'No estas conectado a internet'),
+          );
+        }
+
+        const id = Data.ddp.method(eventName, args);
+
+        Data.calls.push({
+          id: id,
+          callback(error, response) {
+            clearTimeout(timeoutId);
+
+            if (error) {
+              reject(error);
+            } else {
+              resolve(response);
+            }
+          },
+        });
+      })
+      .catch(error => {
+        clearTimeout(timeoutId);
+
+        reject(error);
+      });
+  });
+
+  let timeoutPromise = new Promise((resolve, reject) => {
+    timeoutId = setTimeout(() => {
+      reject(
+        new MeteorError(
+          'TIMEOUT',
+          'El servidor tomo mas de lo esperado en responder',
+        ),
+      );
+    }, config.timeout || 15 * 1000);
+  });
+
+  return Promise.race([callPromise, timeoutPromise]);
 }
